a6a4856cc3870a5813ee4576ee865d05c52e62ae
Praveen Josyula <praveenkumar78952@gmail.com>
2025-03-22 00:45:42 +0530

    Added toggles

--- Client/src/components/Lobby.tsx
+++ Client/src/components/Lobby.tsx
@@ -1,62 +1,89 @@
 import { useEffect, useRef, useState } from "react";
-import { Link, useSearchParams } from "react-router-dom";
+import {useSearchParams } from "react-router-dom";
+import UserCard from "./UserCard";
 
-export default function Lobby() {
-  const [name, setName] = useState<string>("");
+export default  function Lobby() {
   const [searchParameters] = useSearchParams();
-  const type = searchParameters.get("type");
-  const [audostream, setAudioStream] = useState<MediaStreamTrack>();
+  const [camOn, setCamOn] = useState<boolean>(true);
+  const [micOn, setMicOn] = useState<boolean>(true);
+  const [audiostream, setAudioStream] = useState<MediaStreamTrack>();
   const [videoStream, setVideoStream] = useState<MediaStreamTrack>();
   const localVideo = useRef<any>(null);
 
-  const getVideo = async() =>{
-    const stream = await window.navigator.mediaDevices.getUserMedia({video: {width:250,height:250}, audio: true})
-    let videoTrack = stream.getVideoTracks()[0]
-    setAudioStream(stream.getAudioTracks()[0]);
-    setVideoStream(videoTrack);
-    if(!localVideo.current){
+  const AddAudioTrack = (audioTrcak: MediaStreamTrack) => {
+    if (!micOn){
+      audiostream?.stop();
+      setAudioStream(undefined);
       return;
     }
+
+    setAudioStream(audioTrcak)
+  }
+
+  const AddVideoTrack = (videoTrack: MediaStreamTrack) => {
+    if (!camOn){
+      videoStream?.stop();
+      setVideoStream(undefined);
+      return;
+    }
+    
+    setVideoStream(videoTrack)
+  }
+
+  const EndAllMediaTracks = (mediaStream: MediaStream | null) => {
+    audiostream?.stop();
+    videoStream?.stop();
+
+    mediaStream?.getTracks().forEach(x=>{
+      if(x.readyState == 'live'){
+        x.stop();
+      }
+    })
+  }
+
+  const getVideoAndAudio = async() =>{
+    if(!micOn && !camOn){
+      EndAllMediaTracks(localVideo.current?.srcObject);
+      return ;
+    }
+
+    const stream = await window.navigator.mediaDevices.getUserMedia({
+        audio: micOn,
+        video: camOn ? {width:240,height:240} : false
+        })
+
+    AddAudioTrack(stream.getAudioTracks()?.[0])
+    AddVideoTrack(stream.getVideoTracks()?.[0])
+    console.log(stream.getTracks().forEach(x=>{
+      if(x.readyState === 'live'){
+        console.log(x)
+      }
+    }))
+
+    if(!localVideo.current || !camOn){
+      return;
+    }
+
+    let videoTrack = stream.getVideoTracks()[0]
     localVideo.current.srcObject =  new MediaStream([videoTrack]);
   }
 
   useEffect(()=>{
-    if(localVideo && localVideo.current){
-      getVideo();
-    }
-  },[localVideo])
+    getVideoAndAudio();
+  },[localVideo, micOn, camOn])
 
   return (
     <div className="flex flex-row gap-5 items-center justify-center min-h-screen bg-gray-100">
-      {type === "video"?
+      {searchParameters.get("type") === "video"?
         <div>
-          <video ref={localVideo} autoPlay></video>
-          <button>cam</button>
-          <button>mic</button>
+          {camOn ? <video ref={localVideo} autoPlay ></video> : <div style={{width:240,height:240,background:"#444"}}></div>}
+          <button onClick={()=>setCamOn(prev => !prev)} className="mt-6 inline-block w-full px-6 py-2 text-white font-semibold bg-gradient-to-r from-blue-500 to-indigo-600 rounded-full shadow-md transition-transform transform hover:scale-105">cam</button>
+          <button onClick={()=>setMicOn(prev => !prev)} className="mt-6 inline-block w-full px-6 py-2 text-white font-semibold bg-gradient-to-r from-blue-500 to-indigo-600 rounded-full shadow-md transition-transform transform hover:scale-105">mic</button>
         </div>
         :
         <></>
       }
-      <div className="flex flex-col bg-white rounded-2xl shadow-lg p-8 w-[90%] max-w-md text-center">
-        <h2 className="text-2xl font-semibold text-gray-900">Enter Your Name</h2>
-        
-        <input
-          type="text"
-          className="mt-4 w-full px-4 py-2 border rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none"
-          placeholder="Enter your name..."
-          onChange={(e) => setName(e.target.value)}
-          autoFocus
-        />
-
-        <Link
-          to={name ? `/${type}?name=${name}` : "#"}
-          className={`mt-6 inline-block w-full px-6 py-2 text-white font-semibold bg-gradient-to-r from-blue-500 to-indigo-600 rounded-full shadow-md transition-transform transform hover:scale-105 ${
-            !name && "opacity-50 pointer-events-none"
-          }`}
-        >
-          Join
-        </Link>
-      </div>
+      <UserCard type={searchParameters.get("type")}/>
     </div>
   );
 }
